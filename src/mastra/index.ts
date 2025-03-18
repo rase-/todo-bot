f0bf622fb12e5f5e4ec3e09cda0bdfa251a29878
Tony Kovanen <tony.kovanen@gmail.com>
2025-03-18 10:47:41 +0200

    put back remote db

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -6,31 +6,31 @@ import { createLogger } from "@mastra/core/logger";
 
 import { memoryAgent } from "./agents";
 
-// const storage = new DefaultStorage({
-//   config: {
-//     url: "libsql://c01c3455-3745-4087-a75b-44721ed68b19-mastra.turso.io",
-//     authToken:
-//       "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzk4MTAwNzAsInAiOnsicnciOnsibnMiOlsiMWIyZjYyMTgtOTI1MS00ZDk3LWIzMDctNzBkMTk3OGIyMjJmIl19fX0.0FqTfjNGPMGk9-Da_0-2cpS8B71uoyMmWnHriQg6ygFJvNNvCgZkKQYJIcrUWG32Df137sf1Knt2qU7uMBj0Bw",
-//   },
-// });
-// await storage.init();
-
-// const vector = new DefaultVectorDB({
-//   connectionUrl:
-//     "libsql://c01c3455-3745-4087-a75b-44721ed68b19-mastra.turso.io",
-//   authToken:
-//     "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzk4MTAwNzAsInAiOnsicnciOnsibnMiOlsiMWIyZjYyMTgtOTI1MS00ZDk3LWIzMDctNzBkMTk3OGIyMjJmIl19fX0.0FqTfjNGPMGk9-Da_0-2cpS8B71uoyMmWnHriQg6ygFJvNNvCgZkKQYJIcrUWG32Df137sf1Knt2qU7uMBj0Bw",
-// });
-
 const storage = new DefaultStorage({
   config: {
-    url: "file:test.db",
+    url: "libsql://c01c3455-3745-4087-a75b-44721ed68b19-mastra.turso.io",
+    authToken:
+      "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzk4MTAwNzAsInAiOnsicnciOnsibnMiOlsiMWIyZjYyMTgtOTI1MS00ZDk3LWIzMDctNzBkMTk3OGIyMjJmIl19fX0.0FqTfjNGPMGk9-Da_0-2cpS8B71uoyMmWnHriQg6ygFJvNNvCgZkKQYJIcrUWG32Df137sf1Knt2qU7uMBj0Bw",
   },
 });
+await storage.init();
 
 const vector = new DefaultVectorDB({
-  connectionUrl: "file:test.db",
+  connectionUrl:
+    "libsql://c01c3455-3745-4087-a75b-44721ed68b19-mastra.turso.io",
+  authToken:
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mzk4MTAwNzAsInAiOnsicnciOnsibnMiOlsiMWIyZjYyMTgtOTI1MS00ZDk3LWIzMDctNzBkMTk3OGIyMjJmIl19fX0.0FqTfjNGPMGk9-Da_0-2cpS8B71uoyMmWnHriQg6ygFJvNNvCgZkKQYJIcrUWG32Df137sf1Knt2qU7uMBj0Bw",
 });
+
+// const storage = new DefaultStorage({
+//   config: {
+//     url: "file:test.db",
+//   },
+// });
+
+// const vector = new DefaultVectorDB({
+//   connectionUrl: "file:test.db",
+// });
 
 export const memory = new Memory({ storage, vector });
 
